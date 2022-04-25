f437644372707a91208d9063879ead2221ef42ab
Maria Saavedra <cotesaavedra@github.com>
2022-04-25 19:46:40 -0400

    Se creó la función para crear un objeto de propiedades por cada URL

--- mdLinks.js
+++ mdLinks.js
@@ -2,43 +2,52 @@ const fs = require('fs')
 const path = require('path');
 const markdownLinkExtractor = require('markdown-link-extractor');
 const { promisify } = require('util');
+const axios = require('axios');
 
-const route = "file";
+const route = "files";
 
 // export const mdLinks = (route) => {
-const mdLinks = (route) => {
-    // return new Promise((resolve, reject) => {
-    //¿LA RUTA ES VALIDA?
-    //-----------VALIDANDO SI ES UNA RUTA EXISTENTE O NO EN EL SISTEMA DE ARCHIVOS--------------
+const mdLinks = (route, options) => {
+    return new Promise((resolve, reject) => {
+        // if (options == 'true') {
+        //     const properties = routeExistence(route)
+
+        // }
+
+        resolve(routeExistence(route))
+        //¿LA RUTA ES VALIDA?
+        //IDENTIFICAR ARCHIVOS MD
+        //LEER AQUELLOS ARCHIVOS MD Y BUSCAR URL
+        // CREAR UN ARRAY DE OBJETOS, CADA OBJETO ES UNA URL CON HREF FILE TEXT STATUS Y OK/FAIL
+        //POR CADA ARCHIVO MD QUE CONTENGA URL GUARDAR EN UN ARRAY DE OBJETOS
+        // ARRAY DE OBJETOS FINAL
+        // Y CUANDP YA ESTÉ RESUELTO... FINALIZA CON RESOLVE QUE CONTIENE UN ARRAY DE OBJETOS
+        //CUANDO OPTIONS ES FALSE EL ARRAY DE OBJETOS SOLO DEVUELVE HREF, FILE Y TEXT, CUANDO ES TRUE AGREGA STATUS Y OK
+        // CUANDO OPTIONS ES -STATS DEVUELVE TOTAL DE URLs Y URLs UNICAS
+        // CUANDO OPTIONS ES AMBOS DEVUELVE TOTAL DE URLs, URLs UNICAS Y URLs ROTAS
+        // resolve()
+        ///...
+        //...
+        reject(err)
+    });
+}
+//---FUNCION PARA IDENTIFICAR SI LA RUTA EXISTE O NO EN EL SISTEMA DE ARCHIVOS---
+const routeExistence = (route) => {
     const exists = promisify(fs.exists);
     exists(route)
         .then((exist) => {
             if (exist) {
                 console.log('Route exists')
                 //¿ES UN DIRECTORIO O UN ARCHIVO? - RECURSIVIDAD
-                identify(route)
+                identify(route) //RETORNA UN OBJETO PROVENIENTE DE SEARCHURL
+
                 //llamar a la función de ¿Es un directorio?
             } else {
                 //ruta no existe en el sistema de archivos
                 console.log('Route does not exist')
             }
         })
-    //IDENTIFICAR ARCHIVOS MD
-    //LEER AQUELLOS ARCHIVOS MD Y BUSCAR URL
-    // CREAR UN ARRAY DE OBJETOS, CADA OBJETO ES UNA URL CON HREF FILE TEXT STATUS Y OK/FAIL
-    //POR CADA ARCHIVO MD QUE CONTENGA URL GUARDAR EN UN ARRAY DE OBJETOS
-    // ARRAY DE OBJETOS FINAL
-    // Y CUANDP YA ESTÉ RESUELTO... FINALIZA CON RESOLVE QUE CONTIENE UN ARRAY DE OBJETOS
-    //CUANDO OPTIONS ES FALSE EL ARRAY DE OBJETOS SOLO DEVUELVE HREF, FILE Y TEXT, CUANDO ES TRUE AGREGA STATUS Y OK
-    // CUANDO OPTIONS ES -STATS DEVUELVE TOTAL DE URLs Y URLs UNICAS
-    // CUANDO OPTIONS ES AMBOS DEVUELVE TOTAL DE URLs, URLs UNICAS Y URLs ROTAS
-    // resolve()
-    ///...
-    //...
-    // reject()
-    // });
 }
-
 //-----FUNCION PARA IDENTIFICAR SI ES UN ARCHIVO O UN DIRECTORIO-----------
 const identify = (route) => {
     return new Promise((resolve, reject) => {
@@ -48,32 +57,14 @@ const identify = (route) => {
             }
             if (inf.isFile()) {
                 console.log(`¿Is file?: ${inf.isFile()}`);
-                // extName(route);
-                resolve('File')
+                resolve(extName(route))
             }
             if (inf.isDirectory()) {
                 console.log(`¿Is directory?: ${inf.isDirectory()}`);
-                // directory(route);
-                resolve('Directory')
+                resolve(directory(route))
             }
         })
     })
-        .then((typeRoute) => {
-            if (typeRoute === 'Directory') {
-                //LEER EL DIRECTORIO Y PREGUNTAR SI TIENE ARCHIVOS Y/O DIRECTORIOS
-                directory(route)
-            }
-            if (typeRoute === 'File') {
-                //IDENTIFICAR ARCHIVOS MD
-                extName(route)
-                    .then((mdFile) => {
-                        if (mdFile == '.md') {
-                            searchURL(route)
-
-                        }
-                    })
-            }
-        })
 }
 //-----------------------------------------FILE----------------------------------------
 //-----FUNCION PARA IDENTIFICAR SI ES UN ARCHIVO .MD-------
@@ -83,11 +74,10 @@ const extName = (route) => {
         if (extName != '.md') {
             //no es un archivo .md, no se puede leer
             reject('Path-is-not-a-file-.md');
-        } else {
+        } else if ((extName == '.md')) {
             //leer el archivo md y buscar las url
             console.log('It is a .md archive')
-            resolve('.md');
-            // searchURL(route);
+            resolve(searchURL(route));
         }
     })
 }
@@ -103,34 +93,58 @@ const readFile = (route) => {
         })
     })
 }
+
+//--FUNCION QUE RECORRE TODO EL MD Y POR CADA LINK CREA UN OBJETO CON SUS PROPIEDADES
+const fileRound = (links, routeFile, data) => {
+    links.forEach(function (link) {
+        axios.get(link)
+            .then(function (response) {
+                const URLFile = {
+                    href: link,
+                    text: data.match(/\[([^()]*[^()]*)\]/)[0],
+                    file: routeFile,
+                    Status: response.status,
+                    StatusText: response.statusText,
+                    okFail: 'ok',
+                }
+                console.log(URLFile)
+                return URLFile
+            })
+            .catch(err => {
+                if (err.response) {
+                    const URLFile = {
+                        href: link,
+                        text: data.match(/\[([^()]*[^()]*)\]/)[0],
+                        file: routeFile,
+                        Status: err.response.status,
+                        StatusText: err.response.statusText,
+                        okFail: 'fail',
+                    }
+                    console.log(URLFile)
+                    return URLFile
+                }
+            });
+    })
+}
 //------FUNCIÓN PARA BUSCAR URLs EN UN ARCHIVO .MD--------
 const searchURL = (route) => {
     return new Promise((resolve, reject) => {
         readFile(route)
             .then((data) => {
                 const { links } = markdownLinkExtractor(data);
-                const fileObj = {
-                    file: route,
-                    numeroURLs: links.length,
-                    URLs: links,
-                    //text: data,
+                if (links.length >= 1) {
+
+                    const objectForRoute = fileRound(links, route, data);
+                    //    console.log(objectForRoute);
+                    resolve(objectForRoute)
                 }
-                console.log('objeto del archivo', fileObj)
-                console.log('************')
-                resolve(fileObj)
-                // if (links.length === 0) {
-                //     console.log(`El archivo: ${route} no tiene URLs`)
-                //     resolve(route, data, links)
-                // } else {
-                //     console.log(`Se encontraron ${links.length} URLs en ${route}`)
-                //     resolve(route, links, data)
-                // }
             })
             .catch((err) => {
                 reject(err)
             })
     })
 }
+
 //-----------------------------------DIRECTORY-------------------------------
 //---------FUNCION QUE LEE UN DIRECTORIO / FUNCION RECURSIVA--------
 const directory = (route) => {
